fdb3c11921e87c26fea6d9788fb34fea50deecef
aureliem07 <aureliemaillot07@gmail.com>
2021-08-04 10:30:30 +0200

    add list of strolls on the homepage and add stroll details

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\StrollRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,24 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
-    {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+    public function index(StrollRepository $repo): Response
+    {   
+        $strolls = $repo->findAll();
+
+
+        return $this->render('home/home.html.twig', [
+            'strolls' => $strolls
+        ]);
+    }
+
+    #[Route('/évènement/{id}', name: 'stroll')]
+    public function stroll(int $id, StrollRepository $repo): Response
+    {     
+
+        $stroll = $repo->find($id);
+
+        return $this->render('home/stroll.html.twig', [
+            'stroll' => $stroll
         ]);
     }
 }
