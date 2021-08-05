b3a197a60796e1c54e5b6f714550275d1cc2d60f
aureliem07 <aureliemaillot07@gmail.com>
2021-08-05 22:56:03 +0200

    display the dogs on the user account

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AccountController extends AbstractController
 {
     #[Route('/account', name: 'account')]
     public function account(): Response
     {
-        return $this->render('account/account.html.twig', [
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $user = $this->getUser();
+        $dogs = $user->getDogs();
 
+        return $this->render('account/account.html.twig', [
+            'dogs' => $dogs
         ]);
     }
 
