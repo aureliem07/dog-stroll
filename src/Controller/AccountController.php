6932498e281065cfb0c2804013d3140ab932b6c4
aureliem07 <aureliemaillot07@gmail.com>
2021-08-09 09:47:53 +0200

    add edit and remove buttons for the AccountController

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -19,5 +23,34 @@ class AccountController extends AbstractController
             'dogs' => $dogs
         ]);
     }
+
+    #[Route('/edit-account/{user}', name: 'edit_account')]
+    public function editAccount(EntityManagerInterface $em, User $user, Request $request): Response
+    {
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $message = 'Votre compte a été modifié.';
+            $this->addFlash('sucess', $message);
+            $em->flush();
+            return $this->redirectToRoute('account');
+        }
+            
+        return $this->render('account/editAccount.html.twig', [
+            'userForm' => $form->createView()
+        ]);
+
+    }
+
+    #[Route('/remove-account/{user}', name: 'remove_account')]
+    public function removeAccount(EntityManagerInterface $em, User $user): Response
+    {
+        $em->remove($user);
+        $em->flush();
+            
+        return $this->redirectToRoute('home'); 
+        
+    }
+
 
 }
