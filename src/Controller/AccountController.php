0fb901dc288844991f0c84814bf4adfa743d9788
aureliem07 <aureliemaillot07@gmail.com>
2021-08-26 11:05:30 +0200

    delete dogController and add it to accountController

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Dog;
 use App\Entity\User;
+use App\Form\DogType;
 use App\Entity\Stroll;
-use App\Form\EditAccountType;
 use App\Form\StrollType;
+use App\Form\EditAccountType;
 use App\Service\FileUploader;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -56,6 +58,63 @@ class AccountController extends AbstractController
         $em->flush();
             
         return $this->redirectToRoute('home'); 
+        
+    }
+
+    #[Route('/add-dog/{user}', name: 'add_dog')]
+    public function addDog(FileUploader $fileUploader, EntityManagerInterface $em, User $user, Request $request): Response
+    {
+        $dog = new Dog();
+        $form = $this->createForm(DogType::class, $dog);
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()){
+            $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+            $user = $this->getUser();
+            $dog->setUser($user);
+
+            $pictureFile = $form->get('picture')->getData();
+            if ($pictureFile) {
+                $pictureFileName = $fileUploader->upload($pictureFile);
+                $dog->setPictureFilename($pictureFileName);
+            }
+
+            $em->persist($dog);
+            $em->flush();
+
+            return $this->redirectToRoute('account'); 
+        }
+
+        return $this->render('account/addDog.html.twig', [
+            'dogForm' => $form->createView()
+        ]);
+    }
+    
+    #[Route('/edit-dog/{dog}', name: 'edit_dog')]
+    public function editDog(EntityManagerInterface $em, Dog $dog, Request $request): Response
+    {
+        $form = $this->createForm(DogType::class, $dog);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $message = 'Le chien ' .$dog->getName(). ' a été modifié.';
+            $this->addFlash('sucess', $message);
+            $em->flush();
+            return $this->redirectToRoute('account');
+        }
+            
+        return $this->render('account/editDog.html.twig', [
+            'dogForm' => $form->createView()
+        ]);
+        
+    }
+
+    #[Route('/remove-dog/{dog}', name: 'remove_dog')]
+    public function removeDog(EntityManagerInterface $em, Dog $dog): Response
+    {
+        $em->remove($dog);
+        $em->flush();
+            
+        return $this->redirectToRoute('account'); 
         
     }
 
